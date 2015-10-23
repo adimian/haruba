eaa357bf82c3d052b0accac000ba5cc7e98e7722
Maarten De Paepe <maarten@adimian.com>
2015-10-23 15:40:57 +0200

    added events file

---
+++ haruba/plugins/chmod/events.py
@@ -0,0 +1,21 @@
+import os
+from scandir import scandir
+from .connection import Sender
+from haruba.api import app
+from haruba.signals import new_file_or_folder, browsing
+
+sender = Sender(app.config)
+
+
+@new_file_or_folder.connect_via(app)
+def newly_created(sender_object, path):
+    sender.send([path])
+
+
+@browsing.connect_via(app)
+def browsing(sender_object, path):
+    print("called browsing event")
+    paths = [path]
+    for item in scandir(path):
+        paths.append(os.path.join(path, item.name))
+    sender.send(paths)
